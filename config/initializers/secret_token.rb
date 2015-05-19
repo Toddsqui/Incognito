7ef03669cc91c26a0b2812c88388c31a2b9221ce
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-05-19 21:26:23 +0900

    Initial commit for the Incognito App -- The one and only app that will allow you to disappear from the planet without people getting ofended

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Incognito::Application.config.secret_key_base = 'eec1c1052251a090fc81136c2b8dede1a3372dcf52cc7b6ee35dd81f850a254baa8b061b0d3d39bfcffd775b388a46af956088577d11286078817832c6ba5844'
